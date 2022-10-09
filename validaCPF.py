00735e2a17a6b5ac8edd76c40f36e76ad97d5e29
Alex <aflorenco13@gmail.com>
2022-10-08 23:56:22 -0300

    Refatoramento de nomes de variáveis em acordo com PEP8

--- validaCPF.py
+++ validaCPF.py
@@ -1,36 +1,36 @@
-cpfUsuario = str(input("CPF: "))
-cpfCalculo = cpfUsuario[0:9]
+user_cpf = str(input("CPF: "))
+analyze_cpf = user_cpf[0:9]
 
-multiplicador1 = 10
-multiplicador2 = 11
-somatorio1 = 0
-somatorio2 = 0
+multiplier_1 = 10
+multiplier_2 = 11
+sum_1 = 0
+sum_2 = 0
 
-for digito in cpfCalculo:
-    somador = int(digito) * multiplicador1
-    somatorio1 += somador
-    multiplicador1 -= 1
+for digit in analyze_cpf:
+    adder = int(digit) * multiplier_1
+    sum_1 += adder
+    multiplier_1 -= 1
 
-calculoDigito1 = 11 - (somatorio1 % 11)
+calculate_digit_1 = 11 - (sum_1 % 11)
 
-if calculoDigito1 > 9:
-    digitoVerificador1 = 0
+if calculate_digit_1 > 9:
+    verifying_digit_1 = 0
 else:
-    digitoVerificador1 = calculoDigito1
+    verifying_digit_1 = calculate_digit_1
 
-cpfNovo = cpfCalculo + str(digitoVerificador1)
+new_cpf = analyze_cpf + str(verifying_digit_1)
 
-for digito in cpfNovo:
-    somador = int(digito) * multiplicador2
-    somatorio2 += somador
-    multiplicador2 -= 1
+for digit in new_cpf:
+    adder = int(digit) * multiplier_2
+    sum_2 += adder
+    multiplier_2 -= 1
 
-digitoVerificador2 = 11 - (somatorio2 % 11)
+verifying_digit_2 = 11 - (sum_2 % 11)
 
-cpfNovo += str(digitoVerificador2)
+new_cpf += str(verifying_digit_2)
 
-if cpfNovo == cpfUsuario:
-    print(f"CPF Válido!\nDígitos de verificação: {digitoVerificador1}{digitoVerificador2}")
+if new_cpf == user_cpf:
+    print(f"CPF Válido!\nDígito Verificador: {verifying_digit_1}{verifying_digit_2}")
 
 else:
-    print(f"CPF Inválido!\nDígitos de verificação: {digitoVerificador1}{digitoVerificador2}")
+    print(f"CPF Inválido!\nDígito Verificador: {verifying_digit_1}{verifying_digit_2}")
